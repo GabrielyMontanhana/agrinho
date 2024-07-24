6a8e70da2ce0de99ae9690c5499b2cca5015f385
GabrielyMontanhana <135168909+GabrielyMontanhana@users.noreply.github.com>
2024-07-24 08:06:44 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+function visita() {
+    const width = 800;
+    const height = 600;
+    const left = (screen.width / 2) - (width / 2)
+    const top = (screen.height / 2) - (height / 2)
+    window.open ('visita.html', 'Visita', 'width = ${width}, height = ${height}, top = ${top}, left = ${left}')
+}
